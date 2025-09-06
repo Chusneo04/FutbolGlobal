5c74a5f5e2823d384057d48fa9493c8bb5afbc6d
Chusneo04 <hugoarroyogalle@gmail.com>
2025-09-06 12:15:04 +0200

    Diseñado el navbar superior y el banner

--- frontend/futbolglobal/src/App.vue
+++ frontend/futbolglobal/src/App.vue
@@ -1,30 +1,24 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <component :is="vistaActual" />
+  </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import InicioView from './views/InicioView.vue'
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+export default {
+  name: 'App',
+  components: {
+    InicioView
+  },
+  computed: {
+    vistaActual() {
+      const path = window.location.pathname
+      if (path == '/')
+        return 'InicioView'
+      return 'InicioView'
     }
   }
 }
-</style>
+</script>
